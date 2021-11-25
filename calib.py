778efd45271085337a6787818d13439ec6604075
Kaspar Kallas <kaspar@digitalsputnik.com>
2021-11-25 14:55:27 +0200

    Calibration Updates
    - #18 Added documentation to the top of calib.py file, to help with calibration
    - #19 Added temporary calibration saving instead of crash, temp is restored at the start of calibration and deleted on export

--- calib.py
+++ calib.py
@@ -6,22 +6,56 @@ to start:
 import calib
 c = calib.calib()
 
-1. Calibrate all intesity levels for all WB (1500K-10 000K)
+1. Calibrate all intesity levels for all WB (1500K-10 000K) by changing offset
+c.update(Kelvin,Intensity,offset=(r,g,b,w),Output)
+
+offset is changed in steps, new offset values are added to the previous one.
+
+Examples:
 c.update(c._5600K,c._intensity_100,offset=(0,0,0,0),output=Output)
+c.update(c._5600K,c._intensity_75,offset=(0,0,0,0),output=Output)
+..
+c.update(c._5600K,c._intensity_0,offset=(0,0,0,0),output=Output)
+
+c.update(c._3200K,c._intensity_100,offset=(0,0,0,0),output=Output)
 c.update(c._3200K,c._intensity_75,offset=(0,0,0,0),output=Output)
-    
-3. Export output and copy to the calib.py line 19 LampCalibration variable
+..
+c.update(c._3200K,c._intensity_0,offset=(0,0,0,0),output=Output)
+
+Example output (after using c.update() as shown above):
+lux range: 7220 - 7514
+WB should be +/-10 and dUV +/-0.0010
+New values R,G,B,W: (149, 239, 48, 302)
+
+Every time you call c.update() you are shown the preferred lux range,
+WB(Kelvin) range, dUV range and the new R,G,B,W values.
+compare these values to values shown by Sekonic
+and change offsets using c.update()[shown above]
+until lamp values are in between preferred ranges. 
+
+2. Export output and copy to the calib.py line 51 LampCalibration variable
 c.export()
     
-4. Once saved the edit in Thonny
+3. Once saved the edit in Thonny
 machine.reset()   #...will load the new calibration table
+
+4. To check if calibration was successful, use
+Output.setColor(R,G,B) with any values and check
+Lux, WB and dUV values on the Sekonic
+
+This tutorial and calibration was made using Sekonic c-7000
+To use this model of Sekonic, select Spectrum on the Sekonic main screen
+After that you should be presented with three values [Tcp(Kelvin),dUV,lux]
+To measure current lamp values press the Measure button on the side
+of the device.
 
 '''
 import Render
 import time
+import os
 
 # Placeholder for the final calibration
-LampCalibartion = ((4, 1, 0, 0), (243, 126, 0, 0), (486, 251, 0, 0), (729, 373, 0, 0), (973, 481, 0, 0), (7, 4, 0, 2), (245, 161, 0, 53), (464, 305, 0, 110), (712, 468, 0, 133), (950, 600, 0, 176), (6, 4, 0, 4), (224, 194, 0, 188), (460, 390, 0, 355), (670, 554, 0, 502), (950, 750, 0, 656), (6, 4, 0, 7), (193, 177, 0, 265), (383, 345, 0, 493), (588, 512, 0, 707), (833, 691, 0, 903), (4, 1, 0, 15), (186, 280, 55, 227), (347, 497, 100, 469), (525, 715, 150, 693), (759, 944, 201, 858), (3, 2, 1, 13), (149, 235, 60, 272), (294, 441, 120, 515), (456, 668, 196, 682), (660, 878, 246, 910), (2, 2, 2, 14), (131, 270, 106, 238), (260, 507, 206, 475), (392, 718, 295, 712), (523, 897, 380, 950), (0, 1, 2, 11), (125, 291, 137, 237), (251, 547, 264, 475), (370, 765, 378, 712), (502, 927, 477, 950))
+LampCalibartion = ((4, 1, 0, 0), (243, 126, 0, 0), (486, 251, 0, 0), (729, 373, 0, 0), (973, 481, 0, 0), (7, 4, 0, 2), (245, 161, 0, 53), (464, 305, 0, 110), (712, 468, 0, 133), (950, 600, 0, 176), (6, 4, 0, 4), (224, 194, 0, 188), (460, 390, 0, 355), (670, 554, 0, 502), (950, 750, 0, 656), (6, 4, 0, 7), (193, 177, 0, 265), (383, 345, 0, 493), (588, 512, 0, 707), (833, 691, 0, 903), (4, 1, 0, 15), (186, 280, 55, 227), (347, 497, 100, 469), (525, 715, 150, 693), (759, 944, 201, 858), (3, 2, 1, 13), (149, 239, 48, 302), (295, 453, 95, 555), (474, 695, 149, 742), (660, 917, 255, 910), (2, 2, 2, 14), (131, 270, 106, 238), (260, 507, 206, 475), (392, 718, 295, 712), (523, 897, 380, 950), (0, 1, 2, 11), (125, 291, 137, 237), (251, 547, 264, 475), (370, 765, 378, 712), (502, 927, 477, 950))
 
 class calib():
     #Intensity points are linear scale 0.001%, 25%, 50%, 75% 100%
@@ -61,9 +95,18 @@ class calib():
         self._lx.append([[125,24700],[125, 6268, 12412, 18556, 24700]])
         self._lx.append([[159,26100],[159, 6644, 13129, 19614, 26100]])
         self._lx.append([[196,29300],[196, 7472, 14748, 22024, 29300]])
-        self._lx.append([[190,28900],[190, 7367, 14545, 21722, 28900]])
+        self._lx.append([[190,29300],[190, 7367, 14545, 21722, 28900]])
         self._lx.append([[140,29000],[140, 7355, 14570, 21785, 29000]])
         self._lx.append([[140,29600],[140, 7505, 14870, 22235, 29600]])
+        
+        #Temp calib loading, in case of crash
+        try:
+            from _calibTemp import tempCalib
+            self._colorpoints = tempCalib
+            print("Previous calib settings loaded")
+        except Exception as e:
+            pass
+        #end of temp calib loading
 
     '''Setup
 
@@ -112,15 +155,30 @@ class calib():
         
         self._colorpoints[lightWB][lightInt] = tuple(out)
         print("New values R,G,B,W: "+str(self._colorpoints[lightWB][lightInt]))
+        
+        #Temp calib saving, in case of crash
+        calibTemp = open("_calibTemp.py", "w")
+        calibTemp.write("tempCalib=" + self.get_calib() + "\n")
+        calibTemp.close()
+        #end of temp saving
+        
         if not(output==None):
             output.pushRGBW(out)
             
     def export(self):
+        print(self.get_calib())
+        #removing calib temp
+        try:
+            os.remove("_calibTemp.py")
+        except Exception as e:
+            print("Error:" + str(e))
+        
+    def get_calib(self):
         out = ()
         for i in self._colorpoints:
             out += tuple(i)
         
-        print(str(out))
+        return str(out)
         
     def test(self, output, time_in=0.3):
         for i in range(256):
